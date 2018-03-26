22dbff412fa50a87605e5b0759176eb4f962271c
Riley Adam Voth <contact@rileyadamvoth.com>
2018-03-25 19:07:27 -0500

    Adding starter files to the theme
    These are the foundational files to get us going as a child theme.

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+/***
+ * Child theme functions and definitions.
+ *
+ * Add custom PHP in this file. 
+ ***/
+
+function generatepress_child_enqueue_scripts() {
+	if ( is_rtl() ) {
+		wp_enqueue_style( 'generatepress-rtl', trailingslashit( get_template_directory_uri() ) . 'rtl.css' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'generatepress_child_enqueue_scripts', 100 );
